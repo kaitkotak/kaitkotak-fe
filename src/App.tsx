381b2b150bd34fc2f106360a9cf46b55e77c44d6
rama <kaitkotak.team1@gmail.com>
2025-03-13 19:15:51 +0700

    fix: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Button, ConfigProvider, Divider, Input, Space } from "antd";
+import { ConfigProvider, Space } from "antd";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <ConfigProvider
