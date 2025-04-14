1ef9d88fca59d0f4dd57eed0e5f10f1cd9c17f56
rama <kaitkotak.team1@gmail.com>
2025-04-14 11:27:26 +0700

    fix: restyling production forn

--- src/features/production/pages/productionForm.tsx
+++ src/features/production/pages/productionForm.tsx
@@ -18,18 +18,15 @@ const ProductionForm = () => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
-  const gridStyle: React.CSSProperties = {
-    width: "25%",
-    textAlign: "center",
-  };
   const [productionItems, setProductionItems] = useState<IProductionItem[]>([]);
   const navigate = useNavigate();
   const { mutateAsync: create } = useCreateProduction();
   const { id } = useParams();
   const { setBreadcrumb } = useContext(BreadcrumbContext);
-  const { data } = UseGetProduction(id!);
-  const [isFirstLoad, setIsFirstLoad] = useState<boolean>(true);
+  const { data } = UseGetProduction(id ?? "");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { mutateAsync: update } = useUpdateProduction();
+  const submitButtonText: string = id ? "Simpan Perubahan" : "Proses Produksi";
 
   useEffect(() => {
     if (!localStorage.getItem("productionDate") && !id) {
@@ -47,51 +44,36 @@ const ProductionForm = () => {
   }, []);
 
   useEffect(() => {
-    if (itemResponse) {
-      setItems(itemResponse.data.data);
-      setProductionItems(
-        itemResponse.data.data.map((singleData: IItem) => ({
-          id: singleData.id,
-          quantity: 0,
-          image: singleData.image,
-          name: singleData.item_name,
-          code: singleData.item_code,
-        }))
+    if (!itemResponse?.data?.data) return;
+
+    const baseItems = itemResponse.data.data;
+    const productionItems = baseItems.map((item: IItem) => {
+      const matched = data?.data?.data?.production_items?.find(
+        (prodItem: IProductionItemResponse) => prodItem.item_id === item.id
       );
-    }
-  }, [itemResponse]);
 
-  useEffect(() => {
-    console.log(data, productionItems.length, isFirstLoad);
-    console.log("if", data && productionItems.length && isFirstLoad);
-    if (data && productionItems.length && isFirstLoad) {
-      setProductionItems((prevVal: IProductionItem[]) => {
-        return prevVal.map((prev: IProductionItem) => {
-          const matchedData: IProductionItemResponse =
-            data.data.data.production_items.filter(
-              (singleData: IProductionItemResponse) =>
-                prev.id === singleData.item_id
-            )[0];
+      return {
+        id: item.id,
+        quantity: matched ? matched.quantity : 0,
+        image: item.image,
+        name: item.item_name,
+        code: item.item_code,
+      };
+    });
 
-          if (matchedData) {
-            prev.quantity = matchedData.quantity;
-          }
-          return prev;
-        });
-      });
-
-      setIsFirstLoad(false);
-    }
-  }, [data, productionItems]);
+    setItems(baseItems);
+    setProductionItems(productionItems);
+    setIsLoading(false);
+  }, [itemResponse, data]);
 
   const qtyButtonHandler = (id: number, type: "increase" | "decrease") => {
     const selectedItems: IProductionItem[] = productionItems.map(
       (productionItem: IProductionItem) => {
         if (productionItem.id === id) {
-          if (type === "increase") {
-            productionItem.quantity += 1;
-          } else {
+          if (type === "decrease" && productionItem.quantity > 0) {
             productionItem.quantity -= 1;
+          } else if (type === "increase") {
+            productionItem.quantity += 1;
           }
         }
 
@@ -139,7 +121,7 @@ const ProductionForm = () => {
   };
 
   return (
-    <Spin spinning={isFirstLoad}>
+    <Spin spinning={isLoading}>
       <Content
         style={{
           margin: "24px 16px",
@@ -148,57 +130,62 @@ const ProductionForm = () => {
           borderRadius: borderRadiusLG,
         }}
       >
-        <Space size={16} direction="vertical">
-          <Card>
-            {productionItems.map((item: IProductionItem) => (
-              <Card.Grid hoverable style={gridStyle}>
+        <div className="flex gap-3 mb-6 flex-wrap">
+          {productionItems.map((item: IProductionItem) => (
+            <Card
+              hoverable
+              cover={
                 <img
                   alt="item-image"
                   src={`${import.meta.env.VITE_API_URL}/file/download/${
                     item.image
                   }`}
-                  width={100}
+                  style={{ height: "9.375rem" }}
                 />
-                <p className="text-lg font-bold">{item.name}</p>
-                <p className="text-xs">{item.code}</p>
-
-                <Flex>
-                  <Space size={8}>
-                    <Button
-                      onClick={() => qtyButtonHandler(item.id, "increase")}
-                    >
-                      +
-                    </Button>
-                    <InputNumber
-                      value={item.quantity}
-                      onChange={(val) => manualInputQty(item.id, val)}
-                    />
-                    <Button
-                      onClick={() => qtyButtonHandler(item.id, "decrease")}
-                    >
-                      -
-                    </Button>
-                  </Space>
-                </Flex>
-              </Card.Grid>
-            ))}
-          </Card>
-
-          <Flex gap="middle" justify="end">
-            <Button
-              type="default"
-              variant="outlined"
-              htmlType="button"
-              onClick={back}
+              }
             >
-              Batal
-            </Button>
+              <p className="text-lg font-bold">{item.name}</p>
+              <p
+                className="text-xs mb-4
+              "
+              >
+                {item.code}
+              </p>
 
-            <Button type="primary" onClick={submit}>
-              Proses Produksi
-            </Button>
-          </Flex>
-        </Space>
+              <Flex justify="center">
+                <Space size={8}>
+                  <Button onClick={() => qtyButtonHandler(item.id, "increase")}>
+                    +
+                  </Button>
+                  <InputNumber
+                    value={item.quantity}
+                    min={0}
+                    style={{ textAlign: "center" }}
+                    onChange={(val) => manualInputQty(item.id, val)}
+                  />
+                  <Button onClick={() => qtyButtonHandler(item.id, "decrease")}>
+                    -
+                  </Button>
+                </Space>
+              </Flex>
+            </Card>
+          ))}
+        </div>
+
+        <Flex gap="middle" justify="end">
+          <Button
+            type="default"
+            variant="outlined"
+            htmlType="button"
+            onClick={back}
+          >
+            Batal
+          </Button>
+
+          <Button type="primary" onClick={submit}>
+            {submitButtonText}
+          </Button>
+        </Flex>
       </Content>
     </Spin>
   );
